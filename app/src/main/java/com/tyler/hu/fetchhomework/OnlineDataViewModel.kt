e2c658c828e6376171259e41c941035b76e0c3e9
Tyler Hu <tylerhu@yahoo.com>
2024-06-16 00:17:06 -0700

    Mostl finish; still need to delete print statments

--- app/src/main/java/com/tyler/hu/fetchhomework/OnlineDataViewModel.kt
+++ app/src/main/java/com/tyler/hu/fetchhomework/OnlineDataViewModel.kt
@@ -2,12 +2,28 @@ package com.tyler.hu.fetchhomework
 
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
-class OnlineDataViewModel: ViewModel() {
+class OnlineDataViewModel : ViewModel() {
 
     private val _isLoading = MutableStateFlow(true)
+    // Map of item listId to fetched items with the same listId
+    private val _items =
+        MutableStateFlow<Map<Int, List<OnlineDataRepository.FetchItem>>>(emptyMap())
+    private val _error = MutableStateFlow<String?>(null)
+    val isLoading = _isLoading.asStateFlow()
+    val fetchedItems = _items.asStateFlow()
+    val error = _error.asStateFlow()
 
-    suspend fun fetchData(){
-        OnlineDataRepository().getItems()
+    suspend fun fetchData() {
+        _error.value = null
+        _isLoading.value = true
+        try {
+            _items.value = OnlineDataRepository().getItems()
+        } catch (e: Exception) {
+            _error.value = e.message
+        } finally {
+            _isLoading.value = false
+        }
     }
 }
